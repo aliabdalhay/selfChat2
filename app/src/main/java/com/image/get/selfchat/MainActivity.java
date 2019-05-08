dfb6be3f182e6fe01547ad771e3b5b2ab25850e4
iamaliabdelhai <ali.abdalhay@mail.huji.ac.il>
2019-05-09 00:48:33 +0300

    first commit

--- app/src/main/java/com/image/get/selfchat/MainActivity.java
+++ app/src/main/java/com/image/get/selfchat/MainActivity.java
@@ -2,7 +2,11 @@ package com.image.get.selfchat;
 
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Message;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,21 +14,42 @@ import android.content.Context;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements ClickOnMsg{
 
-    private int length_of_list;
+public class MainActivity extends AppCompatActivity implements ClickOnMsg {
+
+    int length_of_list;
     final Gson gson = new Gson();
     Button sendButton;
     EditText messageEditText;
     RecyclerView recyclerView;
     MyAdapter myAdapter;
+    SharedPreferences sp;
+    FirebaseFirestore db;
+    SharedPreferences.Editor editor;
+
+    CollectionReference reference;
 
 
     @Override
@@ -35,18 +60,39 @@ public class MainActivity extends AppCompatActivity implements ClickOnMsg{
         sendButton = (Button) findViewById(R.id.send_btn);
         messageEditText = (EditText) findViewById(R.id.edit_text);
         recyclerView = (RecyclerView) findViewById(R.id.myRecycle);
+
+        FirebaseApp.initializeApp(MainActivity.this);
+        db = FirebaseFirestore.getInstance();
+        reference = db.collection("myDatabase");
+
+
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
         final Toast toast = Toast.makeText(context, "you can't send an empty message, oh silly!", duration);
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        length_of_list = sp.getInt("lst_length", 0);
-        final SharedPreferences.Editor editor = sp.edit();
-        myAdapter = new MyAdapter(this);
+
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sp.edit();
+
+        final int length_of_list = sp.getInt("lst_length", 0);
+
+        myAdapter = new MyAdapter(this, length_of_list, db, sp);
         myAdapter.setClickListener(this);
+
+        new getFireBaseId().execute();
 
 
         recyclerView.setAdapter(myAdapter);
-        myAdapter.checker(length_of_list, sp, gson);
+
+        if (length_of_list != 0){
+            myAdapter.checker(gson);
+        }
+        else if (sp.getBoolean("first_launch", true))
+        {
+            new syncLocalToRemoteFireBase().execute();
+            editor.putBoolean("first_launch", false);
+            editor.apply();
+        }
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
@@ -59,64 +105,51 @@ public class MainActivity extends AppCompatActivity implements ClickOnMsg{
             public void onClick(View v) {
                 String message = messageEditText.getText().toString();
                 messageEditText.setText("");
-                boolean flag = false;
-                if(message.equals(""))
-                {
+                if (message.equals("")) {
                     toast.show();
-                }
-                else
-                {
-                    flag = true;
-                    reserve(editor, flag, message, 0);
+                } else {
+                    new insertDataToFireBase().execute(message);
                 }
             }
         });
     }
 
-    public void reserve(SharedPreferences.Editor editor, boolean flag, String message, int pos){
-        if(flag) {
-            myAdapter.add_message(message);
-            length_of_list += 1;
-        }
-        else
-        {
-            myAdapter.delete_message(pos);
-            length_of_list -= 1;
-        }
-        editor.putInt("lst_length", length_of_list);
-        String wjson = gson.toJson(myAdapter.myDataset);
-        editor.putString("messages_lst", wjson);
-        editor.apply();
 
+    public void saveEditedData() {
+        editor.putInt("data_size", length_of_list);
+        String wjson = gson.toJson(myAdapter.myDataset);
+        editor.putString("sent_messages", wjson);
+        editor.apply();
+        myAdapter.notifyDataSetChanged();
     }
+
+
+
 
     @Override
     public void messageClick(View v, final int pos) {
         DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int poORne) {
-                boolean flag = true;
-                if(poORne == DialogInterface.BUTTON_POSITIVE){
-                    flag = false;
-                    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    final SharedPreferences.Editor editor = sp.edit();
-                    reserve(editor, flag, null, pos);
-                    return;
-                }
-                else
-                {
+                if (poORne == DialogInterface.BUTTON_POSITIVE) {
+                    myAdapter.delete_message(pos);
+                    saveEditedData();
+                    myAdapter.notifyItemRemoved(pos);
+
+                } else {
                     return;
                 }
             }
         };
 
-
-       dialogClick(dialogClickListener, v);
+        dialogClick(dialogClickListener, v);
 
     }
 
 
-    public void dialogClick(DialogInterface.OnClickListener dialogClickListener, View v ){
+
+
+    public void dialogClick(DialogInterface.OnClickListener dialogClickListener, View v) {
         AlertDialog.Builder builder;
         builder = new AlertDialog.Builder(v.getContext());
         builder.setMessage("Are You Sure?").setPositiveButton("Yes", dialogClickListener)
@@ -124,24 +157,103 @@ public class MainActivity extends AppCompatActivity implements ClickOnMsg{
 
     }
 
-
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putStringArrayList("wrote_message", myAdapter.myDataset);
-        super.onSaveInstanceState(outState);
+    protected void onStart()
+    {
+        super.onStart();
+        myAdapter.myDataset = new ArrayList<MyAdapter.Messages>();
+        reference.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                @Nullable FirebaseFirestoreException e) {
+                if(e != null)
+                {
+                    return;
+                }
+                for(DocumentChange documentChange: queryDocumentSnapshots.getDocumentChanges())
+                {
+                    DocumentSnapshot documentSnapshot = documentChange.getDocument();
+                    String id = documentSnapshot.getId();
+                    boolean isDocumentDeleted = documentChange.getOldIndex() != -1;
+                    boolean isDocumentAdded = documentChange.getNewIndex() != -1;
+                    if (isDocumentDeleted)
+                    {
+                        for(int index = 0 ; index < myAdapter.myDataset.size(); index++)
+                            if (myAdapter.myDataset.get(index).Id.equals(id)) {
+                                myAdapter.delete_message(index);
+                                saveEditedData();
+                                myAdapter.notifyItemRemoved(index);
+                                break;
+                            }
+                    }
+
+                    else if(isDocumentAdded && !
+                            documentSnapshot.getId().equals("V7HLvB57aGm5CE0qAeai"))
+                    {
+                        Map<String, Object> new_doc_data = documentSnapshot.getData();
+                        String Id = new_doc_data.get("id")+"";
+                        String content = new_doc_data.get("content")+"";
+                        String timestamp = new_doc_data.get("timestamp")+"";
+                        myAdapter.add_message(Id, timestamp, content);
+                        saveEditedData();
+                        myAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
     }
 
+
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("wrote_message", messageEditText.getText().toString());
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        if(savedInstanceState.getStringArrayList("wrote_message") != null)
-        {
-            myAdapter.myDataset.addAll(savedInstanceState.getStringArrayList("wrote_message"));
+        String wrote_message = savedInstanceState.getString("wrote_message");
+        messageEditText.setText(wrote_message);
+        myAdapter.checker(gson);
+        myAdapter.notifyDataSetChanged();
+
+    }
+
+
+    private class getFireBaseId extends AsyncTask<Void, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            myAdapter.getGlobalId();
+            return null;
         }
     }
+
+    private class insertDataToFireBase extends AsyncTask<String, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(String... strings) {
+            myAdapter.addToRemoteFireBase(strings[0]);
+            return null;
+        }
+    }
+
+    public class syncLocalToRemoteFireBase extends AsyncTask<Void, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            myAdapter.loadDataFromRemoteFireBase(sp, gson);
+            return null;
+        }
+    }
+
+
 }
+
 
 
--- app/src/main/java/com/image/get/selfchat/MyAdapter.java
+++ app/src/main/java/com/image/get/selfchat/MyAdapter.java
@@ -2,22 +2,54 @@ package com.image.get.selfchat;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+
+
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-    public ArrayList<String> myDataset;
+    public ArrayList<Messages> myDataset;
     private LayoutInflater recycleInflater;
     private ClickOnMsg myMsgClick;
+    public FirebaseFirestore db;
+    private SharedPreferences sp;
+    int data_size;
+
+    int my_id = 0;
+
+    public static final String TIME_FORMAT = "kk:mm";
 
 
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener{
@@ -27,6 +59,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             super(v);
             textView = v.findViewById(R.id.textView);
             v.setOnLongClickListener(this);
+
 ;        }
 
 
@@ -42,9 +75,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
 
-    public MyAdapter(Context context){
+    public MyAdapter(Context context, int size, FirebaseFirestore db, SharedPreferences other_sp){
+
         this.recycleInflater = LayoutInflater.from(context);
-        this.myDataset = new ArrayList<String>();
+        this.myDataset = new ArrayList<Messages>();
+        this.db = db;
+        this.sp = other_sp;
+        this.data_size = size;
     }
 
     @Override
@@ -57,40 +94,203 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        String str = myDataset.get(position);
+        String str = myDataset.get(position).Content;
         holder.textView.setText(str);
     }
 
     @Override
     public int getItemCount() {
+        if(myDataset == null)
+        {
+            myDataset = new ArrayList<>();
+            return 0;
+        }
         return myDataset.size();
     }
 
 
-    public void add_message(String msg){
-        this.myDataset.add(msg);
-        this.notifyDataSetChanged();
+    public void add_message(String id, String timestamp, String message){
+        this.myDataset.add(new Messages(id, timestamp, message));
+        data_size += 1;
     }
 
     public void delete_message(int pos){
+        new DeleteDataFromFireBase().execute(this.myDataset.get(pos).Id);
         this.myDataset.remove(pos);
-        this.notifyItemRemoved(pos);
+        data_size -= 1;
     }
+
 
 
     void setClickListener(ClickOnMsg itemClick) {
         this.myMsgClick = itemClick;
     }
 
-    public void checker(int length_of_list, SharedPreferences sp, Gson gson){
 
-        if (length_of_list != 0)
-        {
-            String rjson = sp.getString("messages_lst", "");
-            Type type = new TypeToken<List<String>>() {}.getType();
-            this.myDataset = gson.fromJson(rjson, type);
+    public void checker(Gson gson) {
+        String rjson = sp.getString("sent_messages", "");
+        Type type = new TypeToken<List<Messages>>() {
+        }.getType();
+        this.myDataset = gson.fromJson(rjson, type);
+
+
+    }
+
+    public static String getTime() {
+        DateFormat dateFormat = new SimpleDateFormat(TIME_FORMAT);
+        return dateFormat.format(new Date());
+    }
+
+
+    public class Messages {
+        public String Id, Timestamp, Content;
+
+        public Messages(String Id, String Timestamp, String Content) {
+            this.Content = Content;
+            this.Timestamp = Timestamp;
+            this.Id = Id;
+        }
+    }
+
+
+    public void addToRemoteFireBase(final String message)
+    {
+        String currentTime = getTime();
+
+        DocumentReference washingtonRef = db.collection("myDatabase").
+                document("V7HLvB57aGm5CE0qAeai");
+
+        washingtonRef
+                .update("my_id", my_id + 1)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("", "DocumentSnapshot successfully updated!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("", "Error updating document", e);
+                    }
+                });
+        Map<String, Object> sent_message = new HashMap<>();
+        int increment_id = my_id + 1;
+
+        sent_message.put("content", message);
+        sent_message.put("timestamp",currentTime);
+        sent_message.put("id", increment_id);
+
+        db.collection("myDatabase")
+                .document(increment_id + "")
+                .set(sent_message)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(" ", "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(" ", "Error writing document", e);
+                    }
+                });
+        my_id++;
+
+    }
+
+
+
+
+    public void getGlobalId()
+    {
+        DocumentReference docRef = db.collection("myDatabase").
+                document("V7HLvB57aGm5CE0qAeai");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        final String id = document.getData().get("my_id") + "";
+                        my_id = Integer.parseInt(id);
+                    } else {
+                        Log.d("", "No such document");
+                    }
+                } else {
+                    Log.d("", "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+
+    public void deleteDocument(String doc_id)
+    {
+        db.collection("myDatabase").document(doc_id)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(" ", "DocumentSnapshot successfully deleted!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(" ", "Error deleting document", e);
+                    }
+                });
+    }
+
+
+    public void loadDataFromRemoteFireBase(final SharedPreferences sp, final Gson gson)
+    {
+        final ArrayList<Messages> d = new ArrayList<Messages>();
+        db.collection("myDatabase")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String id, timestamp, content;
+                            Map<String, Object> one_message;
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if(!document.getId().equals("V7HLvB57aGm5CE0qAeai"))
+                                {
+                                    one_message = document.getData();
+                                    id = one_message.get("id") + "";
+                                    timestamp = one_message.get("timestamp") + "";
+                                    content = one_message.get("content") + "";
+                                    d.add(new Messages(id, timestamp, content));
+                                }
+                            }
+
+                            for (Messages m: d)
+                                add_message(m.Id, m.Timestamp, m.Content);
+                            checker(gson);
+
+                        } else {
+                            Log.d(" ", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+
+    private class DeleteDataFromFireBase extends AsyncTask<String, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(String... strings) {
+            deleteDocument(strings[0]);
+            return null;
         }
     }
 
 
 }
+
+
